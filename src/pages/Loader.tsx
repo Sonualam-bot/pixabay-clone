9406deaf1403a643fa99671040a3d63eaff09573
Sonualam <sonualam1599@gmail.com>
2024-01-19 23:51:04 +0530

    refactor

--- src/pages/Loader.tsx
+++ src/pages/Loader.tsx
@@ -2,7 +2,7 @@ function Loader() {
   return (
     <div className="grid min-h-screen w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
       <svg
-        className="w-16 h-16 animate-spin text-gray-900/50"
+        className="w-16 h-16 animate-spin text-green-600"
         viewBox="0 0 64 64"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -22,7 +22,7 @@ function Loader() {
           stroke-width="5"
           stroke-linecap="round"
           stroke-linejoin="round"
-          className="text-gray-900"
+          className="text-gray-600"
         ></path>
       </svg>
     </div>
